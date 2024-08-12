e567fe2ea7ab450488c323668c2438f89e6ed0f0
dzwduan <dzwduan@gmail.com>
2024-08-12 14:59:48 +0800

    fix declare

--- cdp_ede_local/mycpu_env/myCPU/mycpu_top.v
+++ cdp_ede_local/mycpu_env/myCPU/mycpu_top.v
@@ -111,7 +111,7 @@ wire [31:0] alu_src2   ;
 wire [31:0] alu_result ;
 
 wire [31:0] mem_result;
-
+wire [31:0] final_result;
 assign seq_pc       = pc + 3'h4;
 assign nextpc       = br_taken ? br_target : seq_pc;
 
