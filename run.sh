dadeefeb05230b6249293f8fc92aca6fcb1f8c32
dzwduan <dzwduan@gmail.com>
2024-08-13 21:29:25 +0800

    update run script

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-vivado /home/dzw/tools/Xilinx/Vivado/2019.2/bin/run.tcl
+#!/bin/bash
+
+# 指定Vivado的路径
+VivadoPath="/home/dzw/tools/Xilinx/Vivado/2019.2/bin/"
+
+# 指定要运行的Tcl脚本文件路径
+TclScript="run.tcl"
+
+# 运行Vivado并执行Tcl脚本
+$VivadoPath/vivado -mode tcl -source $VivadoPath/$TclScript
