6056b7645be93f233f7d1cda0274d990ede29b8d
dzwduan <dzwduan@gmail.com>
2024-08-14 10:26:53 +0800

    remove unrelated

--- cdp_ede_local/mycpu_env/myCPU/mycpu_top.v
+++ cdp_ede_local/mycpu_env/myCPU/mycpu_top.v
@@ -1,24 +1,25 @@
 `include "mycpu.vh"
+`default_nettype none
 module mycpu_top (
-    input          clk,
-    input          resetn,
+    input  wire         clk,
+    input  wire         resetn,
     // inst sram interface
-    output         inst_sram_en,
-    output [3 : 0] inst_sram_we,
-    output [ 31:0] inst_sram_addr,
-    output [ 31:0] inst_sram_wdata,
-    input  [ 31:0] inst_sram_rdata,
+    output wire         inst_sram_en,
+    output wire [3 : 0] inst_sram_we,
+    output wire [ 31:0] inst_sram_addr,
+    output wire [ 31:0] inst_sram_wdata,
+    input  wire [ 31:0] inst_sram_rdata,
     // data sram interface
-    output         data_sram_en,
-    output [  3:0] data_sram_we,
-    output [ 31:0] data_sram_addr,
-    output [ 31:0] data_sram_wdata,
-    input  [ 31:0] data_sram_rdata,
+    output wire         data_sram_en,
+    output wire [  3:0] data_sram_we,
+    output wire [ 31:0] data_sram_addr,
+    output wire [ 31:0] data_sram_wdata,
+    input  wire [ 31:0] data_sram_rdata,
     // trace debug interface
-    output [ 31:0] debug_wb_pc,
-    output [  3:0] debug_wb_rf_we,
-    output [  4:0] debug_wb_rf_wnum,
-    output [ 31:0] debug_wb_rf_wdata
+    output wire [ 31:0] debug_wb_pc,
+    output wire [  3:0] debug_wb_rf_we,
+    output wire [  4:0] debug_wb_rf_wnum,
+    output wire [ 31:0] debug_wb_rf_wdata
 );
   reg reset;
   always @(posedge clk) reset <= ~resetn;
@@ -31,6 +32,8 @@ module mycpu_top (
   wire                         ds_to_es_valid;
   wire                         es_to_ms_valid;
   wire                         ms_to_ws_valid;
+
+
   wire [`FS_TO_DS_BUS_WD -1:0] fs_to_ds_bus;
   wire [`DS_TO_ES_BUS_WD -1:0] ds_to_es_bus;
   wire [`ES_TO_MS_BUS_WD -1:0] es_to_ms_bus;
@@ -58,78 +61,78 @@ module mycpu_top (
 
 
 
-  id_stage u_id_stage (
-      .clk           (clk),
-      .reset         (reset),
-      //allowin
-      .es_allowin    (es_allowin),
-      .ds_allowin    (ds_allowin),
-      //from fs
-      .fs_to_ds_valid(fs_to_ds_valid),
-      .fs_to_ds_bus  (fs_to_ds_bus),
-      //to es
-      .ds_to_es_valid(ds_to_es_valid),
-      .ds_to_es_bus  (ds_to_es_bus),
-      //to fs
-      .br_bus        (br_bus),
-      //to rf: for write back
-      .ws_to_rf_bus  (ws_to_rf_bus)
-  );
+id_stage u_id_stage(
+    .clk                                (clk                       ),
+    .reset                              (reset                     ),
+    //allowin
+    .es_allowin                         (es_allowin                ),
+    .ds_allowin                         (ds_allowin                ),
+    //from fs
+    .fs_to_ds_valid                     (fs_to_ds_valid            ),
+    .fs_to_ds_bus                       (fs_to_ds_bus              ),
+    //to es
+    .ds_to_es_valid                     (ds_to_es_valid            ),
+    .ds_to_es_bus                       (ds_to_es_bus              ),
+    //to fs
+    .br_bus                             (br_bus                    ),
+    //to rf: for write back
+    .ws_to_rf_bus                       (ws_to_rf_bus              )
+);
 
-  exe_stage u_exe_stage (
-      .clk            (clk),
-      .reset          (reset),
-      //allowin
-      .ms_allowin     (ms_allowin),
-      .es_allowin     (es_allowin),
-      //from ds
-      .ds_to_es_valid (ds_to_es_valid),
-      .ds_to_es_bus   (ds_to_es_bus),
-      //to ms
-      .es_to_ms_valid (es_to_ms_valid),
-      .es_to_ms_bus   (es_to_ms_bus),
-      // to data sram
-      .data_sram_en   (data_sram_en),
-      .data_sram_we   (data_sram_we),
-      .data_sram_we   (data_sram_we),
-      .data_sram_addr (data_sram_addr),
-      .data_sram_wdata(data_sram_wdata)
-  );
+exe_stage u_exe_stage(
+    .clk                                (clk                       ),
+    .reset                              (reset                     ),
+    //allowin
+    .ms_allowin                         (ms_allowin                ),
+    .es_allowin                         (es_allowin                ),
+    //from ds
+    .ds_to_es_valid                     (ds_to_es_valid            ),
+    .ds_to_es_bus                       (ds_to_es_bus              ),
+    //to ms
+    .es_to_ms_valid                     (es_to_ms_valid            ),
+    .es_to_ms_bus                       (es_to_ms_bus              ),
+    // to data sram
+    .data_sram_en                       (data_sram_en              ),
+    .data_sram_we                       (data_sram_we              ), 
+    .data_sram_we                       (data_sram_we              ),
+    .data_sram_addr                     (data_sram_addr            ),
+    .data_sram_wdata                    (data_sram_wdata           )
+);
 
-  mem_stage u_mem_stage (
-      .clk            (clk),
-      .reset          (reset),
-      //allowin
-      .ws_allowin     (ws_allowin),
-      .ms_allowin     (ms_allowin),
-      //from es
-      .es_to_ms_valid (es_to_ms_valid),
-      .es_to_ms_bus   (es_to_ms_bus),
-      //to ws
-      .ms_to_ws_valid (ms_to_ws_valid),
-      .ms_to_ws_bus   (ms_to_ws_bus),
-      .data_sram_rdata(data_sram_rdata)
-  );
+mem_stage u_mem_stage(
+    .clk                                (clk                       ),
+    .reset                              (reset                     ),
+    //allowin
+    .ws_allowin                         (ws_allowin                ),
+    .ms_allowin                         (ms_allowin                ),
+    //from es
+    .es_to_ms_valid                     (es_to_ms_valid            ),
+    .es_to_ms_bus                       (es_to_ms_bus              ),
+    //to ws
+    .ms_to_ws_valid                     (ms_to_ws_valid            ),
+    .ms_to_ws_bus                       (ms_to_ws_bus              ),
+    .data_sram_rdata                    (data_sram_rdata           )
+);
 
 
-  wb_stage u_wb_stage (
-      .clk              (clk),
-      .reset            (reset),
-      //allowin
-      .ws_allowin       (ws_allowin),
-      //from ms
-      .ms_to_ws_valid   (ms_to_ws_valid),
-      .ms_to_ws_bus     (ms_to_ws_bus),
-      //to rf: for write back
-      .ws_to_rf_bus     (ws_to_rf_bus),
-      //to ds
-      .ws_to_ds_valid   (ws_to_ds_valid),
-      //trace debug interface
-      .debug_wb_pc      (debug_wb_pc),
-      .debug_wb_rf_we   (debug_wb_rf_we),
-      .debug_wb_rf_wnum (debug_wb_rf_wnum),
-      .debug_wb_rf_wdata(debug_wb_rf_wdata)
-  );
+wb_stage u_wb_stage(
+    .clk                                (clk                       ),
+    .reset                              (reset                     ),
+    //allowin
+    .ws_allowin                         (ws_allowin                ),
+    //from ms
+    .ms_to_ws_valid                     (ms_to_ws_valid            ),
+    .ms_to_ws_bus                       (ms_to_ws_bus              ),
+    //to rf: for write back
+    .ws_to_rf_bus                       (ws_to_rf_bus              ),
+    //to ds
+    .ws_to_ds_valid                     (ws_to_ds_valid            ),
+    //trace debug interface
+    .debug_wb_pc                        (debug_wb_pc               ),
+    .debug_wb_rf_we                    (debug_wb_rf_we          ),
+    .debug_wb_rf_wnum                   (debug_wb_rf_wnum          ),
+    .debug_wb_rf_wdata                  (debug_wb_rf_wdata         )
+);
 
 
 endmodule
